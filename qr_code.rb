4284f42248dae32919af1a99ad389470e61f8829
phhaffner <phhaffner@gmail.com>
2015-10-28 20:08:12 +0100

    bonnes images qr

--- qr_code.rb
+++ qr_code.rb
@@ -1,25 +1,23 @@
-require 'chunky_png'
-require 'rqrcode'
+# require 'chunky_png'
+require 'rqrcode_png'
 
-qrcode = RQRCode::QRCode.new("http://github.com/")
-
-image = qrcode.as_png(
-          resize_gte_to: false,
-          resize_exactly_to: false,
-          fill: 'white',
-          color: 'black',
-          size: 120,
-          border_modules: 4,
-          module_px_size: 6,
-          )
+# image = qrcode.as_png(
+#           resize_gte_to: false,
+#           resize_exactly_to: false,
+#           fill: 'white',
+#           color: 'black',
+#           size: 120,
+#           border_modules: 4,
+#           module_px_size: 6,
+#           )
 
 qr_string = ''
 img_string = ''
 print "Choisissez une phrase à transformer en QR code? "
-qr_string = gets
+qr_string = gets.chomp
 puts "#{qr_string}"
 print "Choisissez un nom à donner à l'image de ce QR code? "
-img_string = gets
+img_string = gets.chomp
 puts "#{img_string}"
 
 qr = RQRCode::QRCode.new( "#{qr_string}", :size => 4, :level => :h )
@@ -28,6 +26,7 @@ qr.modules.each do |row|
         print col ? "X" : " "
     end
     print "\n"
-end
+image = qr.to_img
+image.resize(90, 90).save("qr_img/#{img_string}.png")
 
-image.save( "qr_img/#{img_string}.png", :interlace => true)
+end

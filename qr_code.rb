3b2e976cf94ff726a9fc7bd5579233689c356b40
phhaffner <phhaffner@gmail.com>
2015-10-28 20:11:40 +0100

    nettoyage

--- qr_code.rb
+++ qr_code.rb
@@ -1,15 +1,4 @@
-# require 'chunky_png'
 require 'rqrcode_png'
-
-# image = qrcode.as_png(
-#           resize_gte_to: false,
-#           resize_exactly_to: false,
-#           fill: 'white',
-#           color: 'black',
-#           size: 120,
-#           border_modules: 4,
-#           module_px_size: 6,
-#           )
 
 qr_string = ''
 img_string = ''

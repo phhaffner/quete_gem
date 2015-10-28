722c42ffc3fd5b498a5a176242e16c9f0c71047e
phhaffner <phhaffner@gmail.com>
2015-10-28 17:52:22 +0100

    nom plus court que le code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # quete_gem
+
+Lancer ruby qr_code.rb sur votre terminal
+
+Choisissez une phrase à transformer en QR code et un nom à l'image de ce QR code.
+
+Un QR code s'affiche sur le terminal et une image de ce QR code est envoyé dans le dossier qr_image au nom que vous lui avez donné. 
--- qr_code.rb
+++ qr_code.rb
@@ -14,9 +14,13 @@ image = qrcode.as_png(
           )
 
 qr_string = ''
+img_string = ''
 print "Choisissez une phrase à transformer en QR code? "
 qr_string = gets
 puts "#{qr_string}"
+print "Choisissez un nom à donner à l'image de ce QR code? "
+img_string = gets
+puts "#{img_string}"
 
 qr = RQRCode::QRCode.new( "#{qr_string}", :size => 4, :level => :h )
 qr.modules.each do |row|
@@ -26,4 +30,4 @@ qr.modules.each do |row|
     print "\n"
 end
 
-image.save( "qr_img/#{qr_string}.png", :interlace => true)
+image.save( "qr_img/#{img_string}.png", :interlace => true)

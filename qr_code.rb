2e6fa67b3a9c24b1fcd24d1990bf1364bb72fff6
phhaffner <phhaffner@gmail.com>
2015-10-28 17:30:40 +0100

    question pour choisir son qr et image

--- qr_code.rb
+++ qr_code.rb
@@ -1,16 +1,37 @@
+require 'chunky_png'
 require 'rqrcode'
 
 qrcode = RQRCode::QRCode.new("http://github.com/")
-image = qrcode.as_png
-svg = qrcode.as_svg
-html = qrcode.as_html
-string = qrcode.to_s
+image = qrcode.as_png	
+# string = qrcode.to_s
+
+image = qrcode.as_png(
+          resize_gte_to: false,
+          resize_exactly_to: false,
+          fill: 'white',
+          color: 'black',
+          size: 120,
+          border_modules: 4,
+          module_px_size: 6,
+          )
+
+# image = ChunkyPNG::Image.new(16, 16, ChunkyPNG::Color::TRANSPARENT)
+# image[1,1] = ChunkyPNG::Color.rgba(10, 20, 30, 128)
+# png[2,1] = ChunkyPNG::Color('black @ 0.5')
+image.save('img/qr_code.png', :interlace => true)
+
+qr_string = ''
+print "Choisissez une phrase à transformer en QR code? "
+qr_string = gets
+puts "#{qr_string}"
 
 
-qr = RQRCode::QRCode.new( 'le ruby cest la vie', :size => 4, :level => :h )
+
+qr = RQRCode::QRCode.new( "#{qr_string}", :size => 4, :level => :h )
 qr.modules.each do |row|
     row.each do |col| 
         print col ? "X" : " "
     end
     print "\n"
 end
+

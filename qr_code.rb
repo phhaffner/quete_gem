dc41d5510195a32c35b4f1efc0f932c28aa17a29
phhaffner <phhaffner@gmail.com>
2015-10-29 13:08:53 +0100

    indentation

--- qr_code.rb
+++ qr_code.rb
@@ -1,18 +1,18 @@
 require 'rqrcode_png'
 
 print "Choisissez une phrase à transformer en QR code? "
-	qr_string = gets.chomp
-	puts "#{qr_string}"
+qr_string = gets.chomp
+puts "#{qr_string}"
 print "Choisissez un nom à donner à l'image de ce QR code? "
-	img_string = gets.chomp
-	puts "#{img_string}"
+img_string = gets.chomp
+puts "#{img_string}"
 
 qr = RQRCode::QRCode.new( "#{qr_string}", :size => 4, :level => :h )
-	qr.modules.each do |row|
-	    row.each do |col| 
-	        print col ? "X" : " "
-	    end
-	    print "\n"
+qr.modules.each do |row|
+  row.each do |col| 
+    print col ? "X" : " "
+  end
+	print "\n"
 end
 
 image = qr.to_img

66e86489a1bdf8383826cd3d63b4542f22304b62
phhaffner <phhaffner@gmail.com>
2015-10-29 12:19:56 +0100

    alexandre a tjs raison

--- qr_code.rb
+++ qr_code.rb
@@ -1,21 +1,19 @@
 require 'rqrcode_png'
 
-qr_string = ''
-img_string = ''
 print "Choisissez une phrase à transformer en QR code? "
-qr_string = gets.chomp
-puts "#{qr_string}"
+	qr_string = gets.chomp
+	puts "#{qr_string}"
 print "Choisissez un nom à donner à l'image de ce QR code? "
-img_string = gets.chomp
-puts "#{img_string}"
+	img_string = gets.chomp
+	puts "#{img_string}"
 
 qr = RQRCode::QRCode.new( "#{qr_string}", :size => 4, :level => :h )
-qr.modules.each do |row|
-    row.each do |col| 
-        print col ? "X" : " "
-    end
-    print "\n"
+	qr.modules.each do |row|
+	    row.each do |col| 
+	        print col ? "X" : " "
+	    end
+	    print "\n"
+end
+
 image = qr.to_img
 image.resize(90, 90).save("qr_img/#{img_string}.png")
-
-end
